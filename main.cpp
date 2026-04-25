57c5b0c5b7a5c1111403deac40c1f54aa76f5fb1
Arthur Santos <arthursantoslouro@gmail.com>
2026-04-24 22:07:29 -0400

    tentando arrumar

--- main.cpp
+++ main.cpp
@@ -10,11 +10,17 @@
 std::string arquivo = "";
 std::string linha;
 std::vector<std::string> linhas;
+std::string pch[10] = {"int", "float", "string", "double", "if", "else if", "else", "break", "while"};
+bool higl = false;
+std::string palavra;
+int tamanho = 10;
 
 // criei a funcao pra pedir arquivo e mandar pra string arquivo
 void pedirarquivo() {
     system("clear");
-    std::cout << "\033[32mASL EDITOR\033[0m""\n";
+    std::cout << "\033[32mASL EDITOR\n\033[0m""\n";
+    std::cout << "\033[33m" << arquivo << "\033[0m\n""";
+    std::cout << "_________________________\n"; 
     std::cout << "\ninsira o nome do arquivo:\n";
     std::getline(std::cin, arquivo);
 }
@@ -22,21 +28,32 @@ void pedirarquivo() {
 //aqui crianos pra abrir arquivo e carregar linhas
 void abrirarquivo(std::string arquivo) {
     system("clear");
+    std::cout << "\033[32mASL EDITOR\n\033[0m""\n";
+    std::cout << "arquivo:\033[33m" << arquivo << "\033[0m\n""";
+    std::cout << "_________________________\n";
     std::ifstream meuarquivo(arquivo);
     while (std::getline(meuarquivo, linha)) {
-        std::getline(meuarquivo, linha);
         std::cout << linha << std::endl;
-        
+
     }
 
 
-std::cout << "\033[32mASL EDITOR\033[0m""\n""";
 }
 
 
 
+// tentando fazer uma funcao pra ativar highlight
+void higlight(bool highl) {
+    
+    
 
 
+    for (int n = 0; n < tamanho; n++) {
+        if (palavra == pch[n]) {
+            std::cout << "\033[34m" << palavra << "\033[0m]";
+        }
+    }
+}
 
 
 
@@ -46,5 +63,6 @@ std::cout << "\033[32mASL EDITOR\033[0m""\n""";
 int main() {
     pedirarquivo();
     abrirarquivo(arquivo);
+    higlight(true);
     return 0;
 } 
